622a577df84683ed27937796aac422cf66c3f521
axtk <axtkx@inbox.ru>
2022-08-06 22:14:06 +0300

    move code from index to src

---
+++ src/useProgressiveCallback.ts
@@ -0,0 +1,60 @@
+import {useCallback, DependencyList} from 'react';
+import {schedule, ScheduleOptions} from 'skdl';
+import {isDependencyList} from './isDependencyList';
+import {useMountedState} from './useMountedState';
+
+export type ProgressiveCallbackState = 'pending' | 'fulfilled' | 'rejected' | undefined;
+export type ProgressiveCallbackOptions<T> = ScheduleOptions<T>;
+
+export function useProgressiveCallback<P extends any[], T>(
+    callback: (...args: P) => Promise<T> | T,
+    options?: ProgressiveCallbackOptions<T | undefined>,
+    deps?: DependencyList,
+): [ProgressiveCallbackState, (...args: P) => Promise<T | undefined>];
+
+export function useProgressiveCallback<P extends any[], T>(
+    callback: (...args: P) => Promise<T> | T,
+    deps?: DependencyList,
+): [ProgressiveCallbackState, (...args: P) => Promise<T | undefined>];
+
+export function useProgressiveCallback<P extends any[], T>(
+    callback: (...args: P) => Promise<T> | T,
+    options?: ProgressiveCallbackOptions<T | undefined> | DependencyList,
+    deps?: DependencyList,
+): [ProgressiveCallbackState, (...args: P) => Promise<T | undefined>] {
+    let [callbackState, setCallbackState] = useMountedState<ProgressiveCallbackState>(undefined);
+    let actualOptions: ProgressiveCallbackOptions<T | undefined> | undefined;
+
+    if (isDependencyList(options)) {
+        deps = options;
+        actualOptions = undefined;
+    }
+    else actualOptions = options;
+
+    let enhancedCallback = useCallback(
+        (...args: P): Promise<T | undefined> => {
+            let progressiveCallback = schedule(callback, actualOptions);
+            try {
+                let value = progressiveCallback(...args);
+
+                setCallbackState('pending');
+                return value
+                    .then(resolvedValue => {
+                        setCallbackState('fulfilled');
+                        return resolvedValue;
+                    })
+                    .catch(error => {
+                        setCallbackState('rejected');
+                        throw error;
+                    });
+            }
+            catch (error) {
+                setCallbackState('rejected');
+                throw error;
+            }
+        },
+        deps ?? [],
+    );
+
+    return [callbackState, enhancedCallback];
+}
