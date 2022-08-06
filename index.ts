8f2d3c590871c5699e4bdd55f9e88a3276416fc0
axtk <axtkx@inbox.ru>
2022-08-06 12:26:50 +0300

    add overload without options param

--- index.ts
+++ index.ts
@@ -1,19 +1,41 @@
 import {useState, useCallback, DependencyList} from 'react';
 import {schedule, ScheduleOptions} from 'skdl';
 
+function isDependencyList(value: unknown): value is DependencyList {
+    return Array.isArray(value);
+}
+
 export type ProgressiveCallbackState = 'pending' | 'fulfilled' | 'rejected' | undefined;
 export type ProgressiveCallbackOptions<T> = ScheduleOptions<T>;
 
 export function useProgressiveCallback<P extends any[], T>(
     callback: (...args: P) => Promise<T> | T,
     options?: ProgressiveCallbackOptions<T | undefined>,
-    deps: DependencyList = [],
+    deps?: DependencyList,
+): [ProgressiveCallbackState, (...args: P) => Promise<T | undefined>];
+
+export function useProgressiveCallback<P extends any[], T>(
+    callback: (...args: P) => Promise<T> | T,
+    deps?: DependencyList,
+): [ProgressiveCallbackState, (...args: P) => Promise<T | undefined>];
+
+export function useProgressiveCallback<P extends any[], T>(
+    callback: (...args: P) => Promise<T> | T,
+    options?: ProgressiveCallbackOptions<T | undefined> | DependencyList,
+    deps?: DependencyList,
 ): [ProgressiveCallbackState, (...args: P) => Promise<T | undefined>] {
     let [callbackState, setCallbackState] = useState<ProgressiveCallbackState>();
+    let actualOptions: ProgressiveCallbackOptions<T | undefined> | undefined;
+
+    if (isDependencyList(options)) {
+        deps = options;
+        actualOptions = undefined;
+    }
+    else actualOptions = options;
 
     let enhancedCallback = useCallback(
         (...args: P): Promise<T | undefined> => {
-            let progressiveCallback = schedule(callback, options);
+            let progressiveCallback = schedule(callback, actualOptions);
             try {
                 let value = progressiveCallback(...args);
 
@@ -33,7 +55,7 @@ export function useProgressiveCallback<P extends any[], T>(
                 throw error;
             }
         },
-        [options, ...deps],
+        deps ? [actualOptions, ...deps] : [actualOptions],
     );
 
     return [callbackState, enhancedCallback];
