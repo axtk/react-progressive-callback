82143504ba155b87c48a0c1473e8153b2d5b0090
axtk <axtkx@inbox.ru>
2022-10-01 22:44:31 +0300

    make hook type declaration similar to useCallback

--- src/useProgressiveCallback.ts
+++ src/useProgressiveCallback.ts
@@ -3,13 +3,14 @@ import {useMountedState} from './useMountedState';
 
 export type ProgressiveCallbackState = 'pending' | 'fulfilled' | 'rejected' | undefined;
 
-export function useProgressiveCallback(
-    callback: (...args: any[]) => any,
+export function useProgressiveCallback<T extends Function>(
+    callback: T,
     deps: DependencyList,
-): [ProgressiveCallbackState, typeof callback] {
+): [ProgressiveCallbackState, T] {
     let [callbackState, setCallbackState] = useMountedState<ProgressiveCallbackState>(undefined);
 
-    let enhancedCallback = useCallback<typeof callback>(
+    let enhancedCallback = useCallback<T>(
+        // @ts-ignore the `args` type is the same as in `callback`
         (...args) => {
             try {
                 let value = callback(...args);
